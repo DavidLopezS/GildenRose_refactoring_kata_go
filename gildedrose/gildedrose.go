b27a46c74f29f848ebf0c543275825ff1e38d07d
David López Saludes <david.lopez.saludes@single.earth>
2024-07-05 14:31:49 +0200

    Idiomatic sums and minus improved and two ifs mixed as one

--- gildedrose/gildedrose.go
+++ gildedrose/gildedrose.go
@@ -11,21 +11,16 @@ func UpdateQuality(items []*Item) {
 		if item.Name != "Aged Brie" && item.Name != "Backstage passes to a TAFKAL80ETC concert" {
 			if item.Quality > 0 {
 				if item.Name != "Sulfuras, Hand of Ragnaros" {
-					item.Quality = item.Quality - 1
+					item.Quality--
 				}
 			}
 		} else {
 			if item.Quality < 50 {
 				item.Quality = item.Quality + 1
 				if item.Name == "Backstage passes to a TAFKAL80ETC concert" {
-					if item.SellIn < 11 {
+					if item.SellIn < 11 || item.SellIn < 6 {
 						if item.Quality < 50 {
-							item.Quality = item.Quality + 1
-						}
-					}
-					if item.SellIn < 6 {
-						if item.Quality < 50 {
-							item.Quality = item.Quality + 1
+							item.Quality++
 						}
 					}
 				}
@@ -33,7 +28,7 @@ func UpdateQuality(items []*Item) {
 		}
 
 		if item.Name != "Sulfuras, Hand of Ragnaros" {
-			item.SellIn = item.SellIn - 1
+			item.SellIn--
 		}
 
 		if item.SellIn < 0 {
@@ -41,7 +36,7 @@ func UpdateQuality(items []*Item) {
 				if item.Name != "Backstage passes to a TAFKAL80ETC concert" {
 					if item.Quality > 0 {
 						if item.Name != "Sulfuras, Hand of Ragnaros" {
-							item.Quality = item.Quality - 1
+							item.Quality--
 						}
 					}
 				} else {
@@ -49,7 +44,7 @@ func UpdateQuality(items []*Item) {
 				}
 			} else {
 				if item.Quality < 50 {
-					item.Quality = item.Quality + 1
+					item.Quality++
 				}
 			}
 		}
